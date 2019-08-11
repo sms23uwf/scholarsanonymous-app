2fe87f1b9259c739fabf3aa17491c663beabb2ee
sms23uwf <stevesatterfield2286@gmail.com>
2019-08-10 20:19:52 -0500

    corrections to functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 
+const functions = require('firebase-functions');
+import 'bootstrap';
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
