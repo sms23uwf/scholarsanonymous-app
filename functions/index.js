8c1d36ee5b2c5120720c0a4f3432daa6abcb8b8d
sms23uwf <stevesatterfield2286@gmail.com>
2019-08-16 17:39:45 -0500

    experimenting with advanced services Drive

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,15 @@
 
 const functions = require('firebase-functions');
-import 'bootstrap';
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+//var file = Drive.Files.get
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-//exports.helloWorld = functions.https.onRequest((request, response) => {
-// response.send("Hello from Firebase!");
-//});
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+ response.send(Drive.Files.List());
+});
