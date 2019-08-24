2e8c26faab8775acbeff65443e147b801833a1d9
sms23uwf <stevesatterfield2286@gmail.com>
2019-08-24 18:31:22 -0500

    correcting case sensitivity issue

--- src/components/PersistentDrawerLeft.js
+++ src/components/PersistentDrawerLeft.js
@@ -19,7 +19,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import routes from '../routers/sidebarrouter';
+import routes from '../routers/SidebarRouter';
 
 const drawerWidth = 240;
 
